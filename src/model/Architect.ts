c13d54151f7e391a1b58026d9d02d57e3cdd4eb7
Victor Maia Sarrasqueiro <victormaiasarrasqueiro@gmail.com>
2023-05-12 15:24:56 -0300

    Class-Validator

--- src/model/Architect.ts
+++ src/model/Architect.ts
@@ -1,15 +1,51 @@
+import { IsDateString, IsFirebasePushId, IsIn, IsOptional, MaxLength, MinLength } from 'class-validator';
+
 import { Address } from './Address';
 import { Document } from './Document';
 import { IArchitect } from './interface/IArchitect';
 
 export class Architect implements IArchitect {
+    
+    @IsFirebasePushId()
     uid: string;
+
+    @IsOptional()
+    @MinLength(3, { message: 'Nome precisa ter no mínimo 3 caracteres' })
+    @MaxLength(30, {
+        message: 'Nome precisa ter no máximo 30 caracteres',
+    })
     nickName: string;
+
+    @MinLength(10, { message: 'Nome Completo precisa ter no mínimo 10 caracteres' })
+    @MaxLength(100, {
+        message: 'Documento precisa ter no máximo 100 caracteres',
+    })
     fullName: string;
+
+    @IsOptional()
+    @MinLength(5, { message: 'Email precisa ter no mínimo 5 caracteres' })
+    @MaxLength(100, {
+        message: 'Email precisa ter no máximo 100 caracteres',
+    })
     email: string;
+    
+    @MinLength(8, { message: 'Celular precisa ter no mínimo 8 caracteres' })
+    @MaxLength(15, {
+        message: 'Celular precisa ter no máximo 15 caracteres',
+    })
     cellphone: string;
-    document: Document;
-    address: Address;
+
+    @IsOptional()
+    @IsDateString()
     birthday: Date;
+
+    @IsOptional()
+    @IsIn(['M', 'F'])
     gender: string;
+
+    @IsOptional()
+    document: Document;
+
+    @IsOptional()
+    address: Address;
 }
--- src/model/Document.ts
+++ src/model/Document.ts
@@ -1,12 +1,37 @@
+import { IsDateString, IsIn, IsNumberString, IsOptional, MaxLength, MinLength } from 'class-validator';
 import { DocumentTypeEnum } from 'src/enum/DocumentTypeEnum';
 
 import { IDocument } from './interface/IDocument';
 
 export class Document implements IDocument {
+    
+    @IsIn(['CPF', 'CNPJ', 'PASSPORT'])
     type: DocumentTypeEnum;
+
+    @IsNumberString()
+    @MinLength(6, { message: 'Documento precisa ter no mínimo 6 caracteres' })
+    @MaxLength(16, {
+        message: 'Documento precisa ter no máximo 16 caracteres',
+    })
     number: string;
+
+    @IsOptional()
+    @MaxLength(30, {
+        message: 'Origem do documento precisa ter no máximo 30 caracteres',
+    })
     origin: string;
+
+    @IsOptional()
+    @IsDateString()
     expirationDate: Date;
+
+    @IsOptional()
+    @IsDateString()
     issueDate: Date;
+
+    @IsOptional()
+    @MaxLength(30, {
+        message: 'Complemento precisa ter no máximo 30 caracteres',
+    })
     complement: string;
 }
