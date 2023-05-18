350649d43ae4cd9a2c7611269092faa8d0e568bf
Victor Maia Sarrasqueiro <victormaiasarrasqueiro@gmail.com>
2023-05-18 17:32:11 -0300

    User

---
+++ src/model/User.ts
@@ -0,0 +1,31 @@
+import { IsFirebasePushId, MaxLength, MinLength } from 'class-validator';
+
+import { IUser } from './interface/IUser';
+
+export class User implements IUser {
+    
+    @IsFirebasePushId()
+    @MinLength(10, { message: 'Uid precisa ter no mínimo 10 caracteres' })
+    @MaxLength(15, {
+        message: 'Uild precisa ter no máximo 36 caracteres',
+    })
+    uid: string;
+
+    @MinLength(5, { message: 'Email precisa ter no mínimo 5 caracteres' })
+    @MaxLength(100, {
+        message: 'Email precisa ter no máximo 100 caracteres',
+    })
+    email: string;
+
+    @MinLength(8, { message: 'Celular precisa ter no mínimo 8 caracteres' })
+    @MaxLength(15, {
+        message: 'Celular precisa ter no máximo 15 caracteres',
+    })
+    cellphone: string;
+
+    @MinLength(10, { message: 'Nome Completo precisa ter no mínimo 10 caracteres' })
+    @MaxLength(100, {
+        message: 'Documento precisa ter no máximo 100 caracteres',
+    })
+    fullName: string;
+}
---
+++ src/model/interface/IUser.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+    uid: string;
+    email: string;
+    cellphone: string;
+    fullName: string;
+}
